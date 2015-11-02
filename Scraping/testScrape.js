7007e534d8ca2e914de0ea862bdcaec70863e951
Jesse Lifshitz <jesse.lifshitz@gmail.com>
2015-11-02 18:23:57 -0500

    encapsulated scraping to ue importIo.getReqString(url)

--- Scraping/testScrape.js
+++ Scraping/testScrape.js
@@ -1,2 +1,8 @@
-var express = require('express');
-var app = express();
+var importIo = require('./testImport');
+
+importIo.getReqString("http://www.koshercasual.com/category.asp?id=364");
+
+//returns an object with the result
+//
+//still need to get the url of the next page
+
