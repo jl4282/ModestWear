20ede196452394c78cfe8c19ef2eea20c52bc82b
Jesse Lifshitz <jesse.lifshitz@gmail.com>
2015-11-23 22:20:12 -0500

    relative paths

--- public/js/services/clothing.js
+++ public/js/services/clothing.js
@@ -1,17 +1,17 @@
 app.factory('Clothing', ['$http', function($http){
   var getClothes = function(){
-    $http.get('http://localhost:3000/api/getClothes').success(function(data){
+    $http.get('/api/getClothes').success(function(data){
       console.log(data);
     });
   };
 
   var getClothingBySlug = function(slug){
-    $http.get('http://localhost:3000/api/getClothes/' + slug).success(function(data){
+    $http.get('/api/getClothes/' + slug).success(function(data){
       console.log(data);
     });
   };
   var searchClothing = function(query){
-    $http.get('http://localhost:3000/api/getClothes/search/' + query).success(function(data){
+    $http.get('/api/getClothes/search/' + query).success(function(data){
       console.log(data);
     });
   };
