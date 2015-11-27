6837970bf2d5da3abbb38d574e70b438d7b2216a
katiekroik <7katie@gmail.com>
2015-11-27 15:25:52 -0500

    updated theme

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 var app = angular.module('ModestWear', ['ngRoute', 'ngMaterial'])
 .config(function($mdThemingProvider, $mdIconProvider) {
-  $mdThemingProvider.theme('default');
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('pink');
   $mdIconProvider.defaultFontSet('material-icons');
 });
