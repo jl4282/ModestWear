ab30e9c88bb7684dff0982ba409966c79d0f2fb7
katiekroik <7katie@gmail.com>
2016-03-13 11:42:51 -0400

    started redoing the styles page

--- public/js/controllers/StylesCtrl.js
+++ public/js/controllers/StylesCtrl.js
@@ -9,6 +9,8 @@ app.controller('StylesCtrl', ['$scope', '$location', '$routeParams', function($s
   $scope.styleClick = function(slug){
     console.log('in clickStyle');
     $location.path('style/'+ slug);
+    // TODO : NEED AN API CALL TO POPULATE THE IMAGES 
+    // OR WE CAN CHANGE THE STYLES MODEL IN THE DB -> GET THAT WHEN U RETREIVE THE USER OBJ
   };
 
 
