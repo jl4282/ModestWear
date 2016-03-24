9beb46b1c8b368cd5a7ddd8a120b728f501ee452
katiekroik <7katie@gmail.com>
2016-03-24 11:18:59 -0400

    just making an outline of what is needed for outfits

---
+++ public/js/services/OutfitSrvc.js
@@ -0,0 +1,38 @@
+// TODO : DONT KNOW IF THIS IS GOOD
+'use strict';
+app.factory('OutfitSrvc', ['$http', function($http){
+  var createOutfit = function(name, id, clothingId){
+    return $http.post('/api/outfit/create', {name: name, id: id, clothes: clothingId}).then(function(res){
+      return res;
+    });
+  };
+  var getOutfits = function(id){
+    return $http.get('/api/outfit').then(function(res){
+      return res;
+    });
+  };
+  var getOutfit = function(slug){
+    return $http.get('/api/outfit/' + slug).then(function(res){
+      return res;
+    });
+  };
+  var addToOutfit = function(outfitId, clothingId){
+    console.log(outfitId, clothingId);
+    return $http.post('/api/outfit/add', {outfitId: outfitId, clothingId: clothingId}).then(function(res){
+      return res;
+    });
+  };
+  var removeFromOutfit = function(outfitId, clothingId){
+    return $http.delete('/api/outfit/remove/' + outfitId + '/' + clothingId).then(function(res){
+      return res;
+    });
+  };
+  
+  return {
+    createOutfit : createOutfit,
+    getOutfits : getOutfits,
+    getOutfit : getOutfit,
+    addToOutfit : addToOutfit,
+    removeFromOutfit : removeOutfit
+  };
+}]);
