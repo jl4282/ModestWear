312618db7abccec66f314f47cc2aaba53a9d6ca5
katiekroik <7katie@gmail.com>
2016-03-31 14:32:30 -0400

    got an icon working

--- public/js/app.js
+++ public/js/app.js
@@ -14,5 +14,6 @@ var app = angular.module('ModestWear', ['ngRoute', 'ngMaterial'])
     .icon('add-circle-outline', '../../../node_modules/material-design-icons/content/svg/production/ic_add_circle_outline_48px.svg')
     .icon('link', '../../../node_modules/material-design-icons/content/svg/production/ic_link_48px.svg')
     .icon('basket', '../../../node_modules/material-design-icons/action/svg/production/ic_shopping_basket_48px.svg')
+    .icon('perm_identity', '../../../node_modules/material-design-icons/action/svg/production/ic_perm_identity_48px.svg')
     ;
 });
